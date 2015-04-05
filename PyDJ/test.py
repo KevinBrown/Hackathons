694988064ce717bc57b883b87cdd845164892218
Kevin Brown <kevin.brown@knights.ucf.edu>
2015-04-04 22:46:41 -0700

    fix bugs in metadata manipulation file

---
+++ PyDJ/test.py
@@ -0,0 +1,7 @@
+from MetadataManipulation import MetadataManipulation
+
+# test = MetadataManipulation("~/Hackathons/PyDJ/test.mp3")
+test = MetadataManipulation()
+test.setPath( "/home/kbrown/Hackathons/PyDJ/test.mp3" )
+test.getArtist()
+
